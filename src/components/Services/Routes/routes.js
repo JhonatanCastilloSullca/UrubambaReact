2c40127f4fb45dbc5b05ecb95ab78786c1e82971
Jhonatan Castillo Sullca <jcastillosullca@gmail.com>
2024-12-21 07:45:33 -0500

    21120745

--- src/components/Services/Routes/routes.js
+++ src/components/Services/Routes/routes.js
@@ -12,47 +12,48 @@ const routes = [
         private: true,
         icon: 'MenuIcon',
     },
-    {
-        to: '/roles',
-        text: 'Roles',
-        private: true,
-        icon: 'MenuIcon',
-    },
-    {
-        text: 'Mantenimiento',
-        private: true,
-        icon: 'MenuIcon',
-        subLinks: [
-            { to: '/configuracion/sectores', text: 'Sectores' },
-            { to: '/configuracion/manzanas', text: 'Manzanas' },
-            { to: '/configuracion/vias', text: 'Vías' },
-            { to: '/configuracion/tecnicos', text: 'Tecnicos' },
-        ],
-    },
+    // {
+    //     to: '/roles',
+    //     text: 'Roles',
+    //     private: true,
+    //     icon: 'MenuIcon',
+    // },
+    // {
+    //     text: 'Mantenimiento',
+    //     private: true,
+    //     icon: 'MenuIcon',
+    //     subLinks: [
+    //         { to: '/configuracion/sectores', text: 'Sectores' },
+    //         { to: '/configuracion/manzanas', text: 'Manzanas' },
+    //         { to: '/configuracion/vias', text: 'Vías' },
+    //         { to: '/configuracion/tecnicos', text: 'Tecnicos' },
+    //     ],
+    // },
     {
         text: 'Fichas',
         private: true,
         icon: 'MenuIcon',
         subLinks: [
-            { to: '/fichas/ficha-catalogacion-espacios-publicos', text: 'Ficha de catalogación espacios públicos' },
+            // { to: '/fichas/ficha-catalogacion-espacios-publicos', text: 'Ficha de catalogación espacios públicos' },
             { to: '/fichas/ficha-registro-catalogacion-inmuebles', text: 'Ficha de registro de catalogación de inmuebles' },
             { to: '/fichas/ficha-registro-catalogacion-inmuebles-area-monumental', text: 'Ficha de registro de catalogación de inmuebles del area monumental' },
+            { to: '/fichas/ficha-registro-historico', text: 'Ficha de registro Historico' },
         ],
     },
 
-    {
-        to: '/login',
-        text: 'Progreso de fichas',
-        private: false,
-        publicOnly: true,
-        icon: 'MenuIcon',
-    },
-    {
-        to: '/logout',
-        text: 'Visor de mapas',
-        private: true,
-        icon: 'MenuIcon',
-    },
+    // {
+    //     to: '/login',
+    //     text: 'Progreso de fichas',
+    //     private: false,
+    //     publicOnly: true,
+    //     icon: 'MenuIcon',
+    // },
+    // {
+    //     to: '/logout',
+    //     text: 'Visor de mapas',
+    //     private: true,
+    //     icon: 'MenuIcon',
+    // },
 ];
 
 export default routes;
