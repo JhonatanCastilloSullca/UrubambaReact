76a8c13102ca032e0c4a0204b5ac84adbdaa925e
Jhonatan Castillo Sullca <jcastillosullca@gmail.com>
2025-01-14 17:36:51 -0500

    140120251719

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -83,7 +83,7 @@ function Login() {
                       name="password"
                       id="password"
                       className="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 dark:text-textAdmin-light placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-                      placeholder="password"
+                      placeholder="Password"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                     />
